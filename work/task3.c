181cb017f8bfd8c5c1370cee5b17680212017b93
Kuznetsov.A <mydumpster@mail.ru>
2018-03-06 18:22:18 +0300

    fix 3

--- work/task3.c
+++ work/task3.c
@@ -2,16 +2,14 @@
 
 char * layout(char buf[], int line, int count)
 {
-	int begin = count - line - 1, end = begin + line * 2; 
-	for (int i = 0; i <= end; ++i)
+	int begin = count - line, end = count + line - 1; 
+	for (int i = 0; i < end; ++i)
 	{
 		if (begin <= i)
 			buf[i] = '*';
 		else
 			buf[i] = ' ';
-		
 	}
-	buf[end + 1] = '\0';
+	buf[end] = '\0';
 	return buf;
 }
-
